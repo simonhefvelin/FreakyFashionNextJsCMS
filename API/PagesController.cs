6c3e33a1016437dad8be14006c7c751b6de41956
Simon Hefvelin Stridsberg <simon_nr6@hotmail.com>
2023-12-12 11:38:48 +0100

    Add project files.

---
+++ API/PagesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace FreakyFashion.API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PagesController : UmbracoApiController
+{
+    private readonly UmbracoHelper umbracoHelper;
+
+
+    public PagesController(UmbracoHelper umbracoHelper)
+    {
+        this.umbracoHelper = umbracoHelper;
+    }
+
+
+
+    [HttpGet("init")]
+    public object GetInit()
+    {
+        var home = umbracoHelper.ContentAtRoot().OfType<HomePage>().First();
+
+        var products = home.Descendants<ProductDetailsPage>();
+
+        var response = new
+        {
+            products = products?.Select(x => new
+            {
+                name = x.ProductName,
+                price = x.ProductPrice,
+                description = x.ProductInfo,
+                image = x.ProductImage?.Url(),
+                link = x.Url()
+            }),
+
+
+            link = home.Link?.Select(x => new
+            {
+                link = x.Content.Value("link"),
+                name = x.Content.Value("name")
+            }),
+
+
+
+            hero = new
+            {
+                heading = home.Heading,
+                message = home.Message,
+                image = home.Image.Url(),
+                button = home.Button,
+                buttonLink = home.ButtonLink
+            },
+
+            spots = home.Spots?.Select(x => new
+            {
+                heading = x.Content.Value("heading"),
+                spotImage = x.Content.Value<IPublishedContent>("spotImage")?.Url(),
+                url = x.Content.Value("url")
+            }),
+
+        };
+
+        return (response);
+    }
+
+    //För att hämta en product
+    [HttpGet("products/{slug}")]
+    public object GetProduct(string slug)
+    {
+        var home = umbracoHelper.ContentAtRoot().OfType<HomePage>().First();
+
+        var product = home
+            .Descendants<ProductDetailsPage>()
+            .FirstOrDefault(x => x.UrlSegment == slug);
+
+        if (product is null)
+            return NotFound();
+
+        var result = new
+        {
+            name = product.ProductName,
+            description = product.ProductInfo.ToString(),
+            price = product.ProductPrice,
+            image = product.ProductImage.Url(),
+        };
+
+        return result;
+    }
+}
---
+++ API/ProductsController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace FreakyFashionUmbraco.API;
+
+[Route("api/[controller]")]
+
+
+public class ProductsController : UmbracoApiController
+{
+    private readonly IMediaService mediaService;
+    private readonly IPublishedContentQuery publishedContentQuery;
+    private readonly IContentService contentService;
+    public ProductsController(IPublishedContentQuery publishedContentQuery,
+        IContentService contentService, IMediaService mediaService)
+    {
+        this.publishedContentQuery = publishedContentQuery;
+        this.contentService = contentService;
+        this.mediaService = mediaService;
+
+    }
+
+    
+    [HttpGet]
+    public IEnumerable<ProductDto> GetProducts()
+    {
+
+        var productDetailsPages = publishedContentQuery
+            .ContentAtRoot()
+            .First()
+            .Descendants<ProductDetailsPage>();
+
+        var products = productDetailsPages.Select(x => new ProductDto()
+        {
+            Name = x.ProductName,
+            Image = x.ProductImage?.Url(),
+            Info = x.ProductInfo,
+            Price = x.ProductPrice,
+           
+
+        });
+
+        return products;
+    }
+
+    // Lägg till ny produkt
+    [HttpPost]
+    public ActionResult CreateProduct(CreateProductRequest createProductRequest)
+    {
+        //var products = publishedContentQuery.ContentSingleAtXPath("//productsDetailsPage");
+
+        var products = publishedContentQuery.ContentAtRoot().First().Descendant<ProductPage>();
+
+        var content = contentService.Create(
+          name: createProductRequest.Name,
+          parentId: products.Key,
+          documentTypeAlias: ProductDetailsPage.ModelTypeAlias);
+
+
+
+        content.SetValue("productName", createProductRequest.Name);
+        content.SetValue("productInfo", createProductRequest.Info);
+        content.SetValue("productPrice", createProductRequest.Price);
+
+        var imageProperty = mediaService.GetMediaByPath(createProductRequest.Image);
+
+        if (imageProperty != null)
+        {
+            
+            content.SetValue("productImage", imageProperty.GetUdi());
+        }
+
+        contentService.SaveAndPublish(content);
+
+        return Created("", null);
+    }
+
+    public class CreateProductRequest
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Info { get; set; }
+        public string Price { get; set; }
+    }
+
+    [HttpGet("search")]
+    public ActionResult<IEnumerable<ProductDto>> SearchProducts(string q)
+    {
+        if (string.IsNullOrEmpty(q))
+        {
+            return Ok(new List<ProductDto>());
+        }
+
+        var products = GetProducts();
+
+        var searchResults = products.Where(p =>
+            p.Name.Contains(q, StringComparison.OrdinalIgnoreCase) ||
+            p.Info.Contains(q, StringComparison.OrdinalIgnoreCase)
+            );
+
+
+        return Ok(searchResults);
+    }
+}
+
+
+public class ProductDto
+{
+    public string Image { get; set; }
+    public string Name { get; set; }
+
+    public string Info { get; set; }
+
+    public string Price { get; set; }
+
+}
+
